c963cc905e88979b35b8da0c901187366b1eb51c
Sanjib <sanjib12sinha@gmail.com>
2021-09-07 12:19:56 +0530

    updated with navigation and fonts

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -13,7 +13,7 @@ class Products with ChangeNotifier {
           'https://cdn.pixabay.com/photo/2018/02/24/20/39/clock-3179167_960_720.jpg',
     ),
     Product(
-      id: 'p1',
+      id: 'p2',
       title: 'Shoe with Gears',
       description: 'Shoes paired with excersise accessories.',
       price: 9.99,
@@ -21,7 +21,7 @@ class Products with ChangeNotifier {
           'https://cdn.pixabay.com/photo/2017/07/02/19/24/dumbbells-2465478_960_720.jpg',
     ),
     Product(
-      id: 'p1',
+      id: 'p3',
       title: 'Wedding Dress',
       description: 'A Wedding Dress combining old style and modern look.',
       price: 9.99,
@@ -29,7 +29,7 @@ class Products with ChangeNotifier {
           'https://cdn.pixabay.com/photo/2016/11/14/04/25/bride-1822587_960_720.jpg',
     ),
     Product(
-      id: 'p1',
+      id: 'p4',
       title: 'Classy Hat',
       description: 'A Woman Hat for her fashion statement.',
       price: 9.99,
@@ -37,4 +37,8 @@ class Products with ChangeNotifier {
           'https://cdn.pixabay.com/photo/2017/05/13/12/40/fashion-2309519_960_720.jpg',
     ),
   ];
+
+  Product findById(String id) {
+    return products.firstWhere((product) => product.id == id);
+  }
 }
--- lib/views/shop_app_with_provider.dart
+++ lib/views/shop_app_with_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../views/shop_product_detail_screen.dart';
+import 'shop_product_detail_screen.dart';
 import 'shop_home_page.dart';
 
 class ShopAppWithProvider extends StatelessWidget {
